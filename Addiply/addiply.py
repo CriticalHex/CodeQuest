d837ea24fc0f62a5ef09d2ca94169d98e2f32497
CriticalHex <kubaska05@gmail.com>
2022-02-26 11:10:10 -0700

    Add project files.

---
+++ Addiply/addiply.py
@@ -0,0 +1,12 @@
+import sys
+
+SEPARATOR = " "
+
+cases = int(sys.stdin.readline().rstrip())
+
+for caseNum in range(cases):
+    line = sys.stdin.readline().rstrip()
+    
+    val1, val2 = (float(val) for val in line.split(SEPARATOR))
+
+    print(int(val1 + val2), int(val1 * val2))
