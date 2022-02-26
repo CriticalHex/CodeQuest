b7a9044786e62fc6d1d6d04435fb7c66ab277ee8
CriticalHex <kubaska05@gmail.com>
2022-02-26 13:57:17 -0700

    Solved Around and Around

---
+++ Around_and_Around/around_and_around.py
@@ -0,0 +1,9 @@
+import sys
+import math
+
+cases = int(sys.stdin.readline().rstrip())
+
+for caseNum in range(cases):
+    line = sys.stdin.readline().rstrip()
+    
+    print(round(((math.pi * (int(line) * 2)) + 40075), 1))
