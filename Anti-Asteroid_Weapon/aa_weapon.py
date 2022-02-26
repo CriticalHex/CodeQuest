18923f6e2de4bf3b3824ccebc7d14b132390023b
CriticalHex <kubaska05@gmail.com>
2022-02-26 13:36:57 -0700

    Solved Anti-Asteroid Weapon

---
+++ Anti-Asteroid_Weapon/aa_weapon.py
@@ -0,0 +1,27 @@
+import sys
+import math
+
+def getKey(val):
+    for key, value in ASTEROIDS.items():
+         if val == value:
+             return key
+
+SEPARATOR = " "
+
+
+cases = int(sys.stdin.readline().rstrip())
+
+for caseNum in range(cases):
+    ASTEROIDS = {}
+    DISTANCES = []
+    line = sys.stdin.readline().rstrip()
+
+    for coords in range(int(line)):
+        line = sys.stdin.readline().rstrip()
+        x, y = (int(val) for val in line.split(SEPARATOR))
+        distance = math.sqrt(pow(x, 2) + pow(y, 2))
+        DISTANCES.append(distance)
+        ASTEROIDS.update({line : distance}) 
+    DISTANCES.sort()
+    for i in range(len(DISTANCES)):
+        print(getKey(DISTANCES[i]))
