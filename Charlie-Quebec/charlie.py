1a62d755651959a7089a91acecb599628ac80fa1
Email 6 <loserkid0006@outlook.com>
2022-03-03 12:41:08 -0700

    Solved Charlie-Quebec

--- Charlie-Quebec/charlie.py
+++ Charlie-Quebec/charlie.py
@@ -6,7 +6,7 @@ NATO = {"A": "Alpha", "B":"Bravo", "C":"Charlie", "D":"Delta",
         "E":"Echo", "F":"Foxtrot", "G":"Golf", "H":"Hotel",
         "I":"India", "J":"Juliet", "K":"Kilo", "L":"Lima",
         "M":"Mike", "N":"November", "O":"Oscar", "P":"Papa",
-        "Q":"Quebec", "R":"Romero", "S":"Sierra", "T":"Tango",
+        "Q":"Quebec", "R":"Romeo", "S":"Sierra", "T":"Tango",
         "U":"Uniform", "V":"Victor", "W":"Whiskey", "X":"Xray",
         "Y":"Yankee", "Z":"Zulu"}
 
@@ -18,12 +18,16 @@ for caseNum in range(cases):
         outputString = ""
         line = sys.stdin.readline().rstrip()
         line = line.upper()
-        
-        for i in range(len(line)):
-            if line[i] != " ":
+       
+        for i in range(len(line)-1):
+            if line[i] != " " and line[i+1] != " ":
                 outputString += NATO.get(line[i])
-                
                 outputString += "-"
+            elif line[i] != " ":
+                outputString += NATO.get(line[i])
             else:
                 outputString += " "
+               
+        outputString += NATO.get(line[len(line)-1])
+       
         print(outputString)
