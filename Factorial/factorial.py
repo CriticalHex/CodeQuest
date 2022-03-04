ac0492dc05b0fb9898848cfa4d4408c7c5c3a80e
Email 6 <loserkid0006@outlook.com>
2022-03-04 13:32:58 -0700

    Solved Factorial

---
+++ Factorial/factorial.py
@@ -0,0 +1,11 @@
+import sys
+from math import factorial
+
+SEPARATOR = " "
+
+cases = int(sys.stdin.readline().rstrip())
+
+for caseNum in range(cases):
+    line = int(sys.stdin.readline().rstrip())
+    print(factorial(line))
+    
