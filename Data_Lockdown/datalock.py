73566223f7deac776076e15846ea888b655fe920
CriticalHex <kubaska05@gmail.com>
2022-03-04 10:52:44 -0700

    Solved Data Lockdown

---
+++ Data_Lockdown/datalock.py
@@ -0,0 +1,21 @@
+import sys
+import math
+import string
+
+SEPARATOR = " "
+
+cases = int(sys.stdin.readline().rstrip())
+
+for caseNum in range(cases):
+    logLine = int(sys.stdin.readline().rstrip())
+    
+    for logLines in range(logLine):
+        line = sys.stdin.readline().rstrip()
+        
+        website, dataSize = (str(val) for val in line.split(SEPARATOR))
+        dataSize = int(dataSize)
+
+        if dataSize > 1000:
+            #if website[len(website)-10:len(website)-1] != ".lmco.com":
+            if ".lmco.com" not in website:
+                print(website, dataSize)
