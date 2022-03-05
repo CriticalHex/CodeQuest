0b94a129aa484d2c49a59cacacabdeac5351b58f
CriticalHex <kubaska05@gmail.com>
2022-03-04 17:53:58 -0700

    Working on File Extensions

---
+++ File_Extensions/file_extend.py
@@ -0,0 +1,18 @@
+'''System module'''
+import sys
+
+SEPARATOR = "."
+DICTIONARY = {}
+
+cases = int(sys.stdin.readline().rstrip())
+
+for caseNum in range(cases):
+    LINE = sys.stdin.readline().rstrip()
+    DISCARD, EXTENSION = (str(val) for val in LINE.split(SEPARATOR))
+    print(DICTIONARY.keys())
+    if DICTIONARY.keys() == EXTENSION:
+        print("This is happening")
+        DICTIONARY.update(EXTENSION, 2)
+    else:
+        DICTIONARY.update({EXTENSION:1})
+print(DICTIONARY)
