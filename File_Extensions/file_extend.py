92b5030507d9765630e5dee2e9e282dc315b5dda
Email 6 <loserkid0006@outlook.com>
2022-03-09 11:31:12 -0700

    Solved Fibonacci Byteman and File Extensions

--- File_Extensions/file_extend.py
+++ File_Extensions/file_extend.py
@@ -9,9 +9,11 @@ cases = int(sys.stdin.readline().rstrip())
 for caseNum in range(cases):
     LINE = sys.stdin.readline().rstrip()
     DISCARD, EXTENSION = (str(val) for val in LINE.split(SEPARATOR))
-    print(DICTIONARY.keys())
-    if DICTIONARY.keys() == EXTENSION:
-        DICTIONARY.update(EXTENSION, 2)
+    if EXTENSION in DICTIONARY.keys():
+        temp = int(DICTIONARY.get(EXTENSION))
+        temp += 1
+        DICTIONARY.update({EXTENSION:temp})
     else:
         DICTIONARY.update({EXTENSION:1})
-print(DICTIONARY)
+for val1, val2 in DICTIONARY.items():
+    print(f"{val1} {val2}")
