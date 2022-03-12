3050953f80081f28fdad3f84efc88d2eb52fb24e
CriticalHex <kubaska05@gmail.com>
2022-03-11 22:00:25 -0700

    Solved Foveated Rendering

--- Foveated_Rendering/render.py
+++ Foveated_Rendering/render.py
@@ -1,71 +1,42 @@
 '''System Module'''
 import sys
 
+def render_out(y_pos, x_pos, arr):
+    '''adds 1 to the count of each neighboring cell'''
+    for ypos in range(-1, 2):
+        for xpos in range(-1, 2):
+            if y_pos+ypos >= 0 and y_pos+ypos < ROWS and x_pos+xpos >= 0 and x_pos+xpos < COLS:
+                if arr[y_pos+ypos][x_pos+xpos] != "100":
+                    arr[y_pos+ypos][x_pos+xpos] = "50"
+    for ypos in range(-2, 3):
+        for xpos in range(-2, 3):
+            if y_pos+ypos >= 0 and y_pos+ypos < ROWS and x_pos+xpos >= 0 and x_pos+xpos < COLS:
+                if arr[y_pos+ypos][x_pos+xpos] != "100" and arr[y_pos+ypos][x_pos+xpos] != "50":
+                    arr[y_pos+ypos][x_pos+xpos] = "25"
+    return arr
+
 SEPARATOR = " "
 
 cases = int(sys.stdin.readline().rstrip())
+ROWS = 20
+COLS = 20
 
 for caseNum in range(cases):
     grid = []
     line = sys.stdin.readline().rstrip()
     y, x = (int(val) for val in line.split(SEPARATOR))
-    for i in range(20):
+    for i in range(ROWS):
         grid.append([])
-        for j in range(20):
+        for j in range(COLS):
             grid[i].append("10")
-    for i in range(20):
-        for j in range(20):
+    for i in range(ROWS):
+        for j in range(COLS):
             if (i == y and j == x):
                 grid[i][j] = "100"
-                if i - 1 >= 0:
-                    grid[i - 1][j] = "50"
-                    if j - 1 >= 0:
-                        grid[i - 1][j - 1] = "50"
-                    if j + 1 < 20:
-                        grid[i - 1][j + 1] = "50"
-                if i + 1 < 20:
-                    grid[i + 1][j] = "50"
-                    if j - 1 >= 0:
-                        grid[i + 1][j - 1] = "50"
-                    if j + 1 < 20:
-                        grid[i + 1][j + 1] = "50"
-                if j - 1 >= 0:
-                    grid[i][j - 1] = "50"
-                if j + 1 < 20:
-                    grid[i][j + 1] = "50"
-                #---------------------------------------------------------------------
-                if i - 2 >= 0:
-                    grid[i - 2][j] = "25"
-                    if j - 2 >= 0:
-                        grid[i - 2][j - 1] = "25"
-                        grid[i - 2][j - 2] = "25"
-                    if j + 2 < 20:
-                        grid[i - 2][j + 1] = "25"
-                        grid[i - 2][j + 2] = "25"
-                if i + 2 < 20:
-                    grid[i + 2][j] = "25"
-                    if j - 2 >= 0:
-                        grid[i + 2][j - 1] = "25"
-                        grid[i + 2][j - 2] = "25"
-                    if j + 2 < 20:
-                        grid[i + 2][j + 1] = "25"
-                        grid[i + 2][j + 2] = "25"
-                if j - 2 >= 0:
-                    grid[i][j - 2] = "25"
-                    if y - 1 >= 0:
-                        grid[i - 1][j - 2] = "25"
-                    if y + 1 < 20:
-                        grid[i + 1][j - 2] = "25"
-                    grid[i][j - 2] = "25"
-                if j + 2 < 20:
-                    grid[i][j + 2] = "25"
-                    if y - 1 >= 0:
-                        grid[i - 1][j + 2] = "25"
-                    if y + 1 < 20:
-                        grid[i + 1][j + 2] = "25"
-    for i in range(20):
-        for j in range(20):
-            if j < 19:
+                grid = render_out(i,j,grid)
+    for i in range(ROWS):
+        for j in range(COLS):
+            if j < COLS - 1:
                 print(grid[i][j], end=" ")
             else:
                 print(grid[i][j], end="")
