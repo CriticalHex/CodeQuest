aafe785cf8459e1061d8af17fb560e2bab2ecab5
Email 6 <loserkid0006@outlook.com>
2023-01-10 14:43:41 -0700

    restarting plink

--- plink/plink.py
+++ plink/plink.py
@@ -1,48 +1,49 @@
-'''System Module'''
+"""System Module"""
 import sys
+import math
 
-SEPARATOR = " "
-results = {}
 
-def get_key(val):
-    '''inverse of getValue'''
-    for key, value in results.items():
-        if val == value:
-            return key
-        
-def norec(B):
-    stack = []
-    i = 0
-    j = 0
-    path = ""
-    total = 0
-    while True:
-        if i >= len(B) - 1:
-            results[path] = total + B[i][j]
-        else:
-            stack.append([i + 1, j, path + "L", total + B[i][j]])
-            stack.append([i + 1, j + 1, path + "R", total + B[i][j]])
+class Node:
+    """Graph nodes"""
 
-def plinko(B, i = 0, j = 0, path = "", total = 0):
-    '''Recursive function to determine best plinko path in a given board'''
-    if i >= len(B) - 1:
-        results[path] = total + B[i][j]
-    else:
-        plinko(B, i + 1, j, path + "L", total + B[i][j])
-        plinko(B, i + 1, j + 1, path + "R", total + B[i][j])
-    
-cases = int(sys.stdin.readline().rstrip())
+    def __init__(self) -> None:
+        self.visited = False
+        self.adjacent: list[tuple[int, Node]]  # current score, next node
+        self.score = -math.inf
 
-for caseNum in range(cases):
-    board = []
-    rows = int(sys.stdin.readline().rstrip())
-    for r in range(rows + 1):
-        line = sys.stdin.readline().rstrip()
-        board.append([])
-        for val in line.split(SEPARATOR):
-            board[r].append(int(val))
-    plinko(board)
-    largest = max(results.values())
-    print(get_key(largest), "=", largest)
-    results = {}
-    
+
+def get_line() -> str:
+    """Get rstripped stdin line"""
+    return sys.stdin.readline().rstrip()
+
+
+# def get_key(val):  # type: ignore
+#     """inverse of getValue"""
+#     for key, value in results.items():
+#         if val == value:
+#             return key
+
+
+# def plinko(B, i=0, j=0, path="", total=0):
+#     """Recursive function to determine best plinko path in a given board"""
+#     if i >= len(B) - 1:
+#         results[path] = total + B[i][j]
+#     else:
+#         plinko(B, i + 1, j, path + "L", total + B[i][j])
+#         plinko(B, i + 1, j + 1, path + "R", total + B[i][j])
+
+
+def main():
+    """Main"""
+    cases = int(get_line())
+    for _ in range(cases):
+        board: list[list[int]] = []
+        rows = int(get_line())
+        for row in range(rows + 1):
+            line = get_line()
+            board.append([])
+            for val in line.split(" "):
+                board[row].append(int(val))
+
+
+main()
