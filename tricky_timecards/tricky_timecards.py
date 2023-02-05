d58f03e8af97a7a71ebb63bbf7899e655cce6efb
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2023-02-04 19:16:56 -0700

    solved tricky timecards

--- tricky_timecards/tricky_timecards.py
+++ tricky_timecards/tricky_timecards.py
@@ -1,11 +1,24 @@
-'''System Module'''
+"""System Module"""
 import sys
 
-SEPARATOR = " "
+SEPARATOR = ","
 
 cases = int(sys.stdin.readline().rstrip())
 
 for caseNum in range(cases):
     line = sys.stdin.readline().rstrip()
-    val1, val2 = (float(val) for val in line.split(SEPARATOR))
-    
+    data = list(val for val in line.split(SEPARATOR))
+    employee = data[0]
+    days = data[1:]
+    hours: int = 0
+    minutes: int = 0
+    for day in days:
+        hour, minute = (val for val in day.split(":"))
+        hours += int(hour)
+        minutes += int(minute)
+    hours += int(minutes / 60)
+    minutes %= 60
+    minstr = f"{minutes} minutes" if minutes != 1 else f"{minutes} minute"
+    hourstr = f"{hours} hours" if hours != 1 else f"{hours} hour"
+    timestr = f"{hourstr} {minstr}" if minutes != 0 else f"{hourstr}"
+    print(f"{employee}={timestr}")
