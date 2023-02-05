0ea40489a778bd620062553abd6f8a90eef787cd
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2023-02-04 19:01:22 -0700

    fixed create, solved what is the operation

--- create.bat
+++ create.bat
@@ -4,9 +4,9 @@ set formatted_input=%formatted_input:-=_%
 mkdir %formatted_input%
 set top_dir=%CD%
 cd %formatted_input%
-powershell.exe -C "wget "https://lmcodequestacademy.com/api/static/problems/%*" -O "%formatted_input%.pdf""
-copy "%top_dir%\template.py" "%formatted_input%.py"
-> input.txt
+powershell.exe -C "wget "https://lmcodequestacademy.com/api/static/problems/%*" -O "%formatted_input%.pdf"">nul
+copy "%top_dir%\template.py" "%formatted_input%.py">nul
+break>input.txt
 cd %top_dir%
 call code %formatted_input%\%formatted_input%.py
 call code %formatted_input%\%formatted_input%.pdf
