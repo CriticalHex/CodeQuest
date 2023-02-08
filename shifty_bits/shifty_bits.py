b9b6c28f98c6f34fd6afd97c31983432484b078d
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2023-02-08 10:55:59 -0700

    float and double still need some formatting

--- shifty_bits/shifty_bits.py
+++ shifty_bits/shifty_bits.py
@@ -3,11 +3,6 @@ import sys
 import struct
 import decimal
 
-# im tired so writing this:
-# TODO:
-# start the binary list reversed, current solution cannot operate when start val is 0 as there is no -0.
-# round and correct exponents for float and double
-
 SEPARATOR = " "
 
 cases = int(sys.stdin.readline().rstrip())
@@ -15,12 +10,12 @@ cases = int(sys.stdin.readline().rstrip())
 
 def bin_to_float(b: str):
     """Convert binary string to a float."""
-    bf = int.to_bytes(int(b, 2), 4)  # 8 bytes needed for IEEE 754 binary64.
+    bf = int.to_bytes(int(b, 2), 4)  # 4 bytes needed for IEEE 754 binary32.
     return struct.unpack(">f", bf)[0]
 
 
 def bin_to_double(b: str):
-    """Convert binary string to a float."""
+    """Convert binary string to a double."""
     bf = int.to_bytes(int(b, 2), 8)  # 8 bytes needed for IEEE 754 binary64.
     return struct.unpack(">d", bf)[0]
 
@@ -37,6 +32,7 @@ def better_round(user_in: float, degree: str = "1.0") -> float:
 for caseNum in range(cases):
     hex_in = int(sys.stdin.readline().rstrip(), 16)
     binary = list(f"{hex_in:0>b}")
+    binary.reverse()
     # print(binary)
     n_measurands = int(sys.stdin.readline().rstrip())
     for _ in range(n_measurands):
@@ -44,8 +40,7 @@ for caseNum in range(cases):
         start = int(start)
         size = int(size)
         if datatype == "int":
-            arr = binary[-start - size : -start]
-            arr.reverse()
+            arr = binary[start : start + size]
             val: int = 0
             for i in range(size):
                 if i == size - 1:
@@ -55,15 +50,21 @@ for caseNum in range(cases):
             # print("".join(binary[-start - size : -start]))
             print(val)
         elif datatype == "uint":
-            arr = "".join(binary[-start - size : -start])
+            arr = "".join(reversed(binary[start : start + size]))
             print(int(arr, 2))
         elif datatype == "float":
-            arr = binary[-start - size : -start]
+            arr = reversed(binary[start : start + size])
             s = "".join(arr)
             unrounded = bin_to_float(s)
+            semirounded = better_round(unrounded, "1.00000")
+            rounded = f"{semirounded:>e}"
+            print(rounded)
+            # rounded = str(better_round(unrounded, "1.00000"))
+            # first, second = rounded.split("+")
             print(better_round(unrounded, "1.00000"))
         elif datatype == "double":
-            arr = binary[start:size]  # WRONG, 0 MEANS 0 FROM THE END
+            arr = reversed(binary[start : start + size])
             s = "".join(arr)
             unrounded = bin_to_double(s)
-            print(better_round(unrounded, "1.00000"))
+            assert isinstance(unrounded, float)
+            print(f"{unrounded:.5e}")
