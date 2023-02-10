fe8fcde232f1e2c9cdd7bb7ea0afd24a21238d1a
Email 6 <loserkid0006@outlook.com>
2023-02-10 14:46:45 -0700

    really not sure what the error is

--- shifty_bits/shifty_bits.py
+++ shifty_bits/shifty_bits.py
@@ -1,31 +1,36 @@
 """System Module"""
 import sys
 import struct
-from codecs import decode
+
+# from codecs import decode
 
 SEPARATOR = " "
 
 cases = int(sys.stdin.readline().rstrip())
 
 
-def int_to_bytes(n: int, length: int):  # Helper function
-    """Int/long to byte string.
+# def int_to_bytes(n: int, length: int):  # Helper function
+#     """Int/long to byte string.
 
-    Python 3.2+ has a built-in int.to_bytes() method that could be used
-    instead, but the following works in earlier versions including 2.x.
-    """
-    return decode("%%0%dx" % (length << 1) % n, "hex")[-length:]
+#     Python 3.2+ has a built-in int.to_bytes() method that could be used
+#     instead, but the following works in earlier versions including 2.x.
+#     """
+#     return decode("%%0%dx" % (length << 1) % n, "hex")[-length:]
 
 
 def bin_to_float(b: str):
     """Convert binary string to a float."""
-    bf = int.to_bytes(int(b, 2), 4)  # 4 bytes needed for IEEE 754 binary32.
+    bf = int.to_bytes(
+        int(b, 2), 4, byteorder="big"
+    )  # 4 bytes needed for IEEE 754 binary32.
     return struct.unpack(">f", bf)[0]
 
 
 def bin_to_double(b: str):
     """Convert binary string to a double."""
-    bf = int.to_bytes(int(b, 2), 8)  # 8 bytes needed for IEEE 754 binary64.
+    bf = int.to_bytes(
+        int(b, 2), 8, byteorder="big"
+    )  # 8 bytes needed for IEEE 754 binary64.
     return struct.unpack(">d", bf)[0]
 
 
@@ -51,7 +56,6 @@ for caseNum in range(cases):
     hex_in = int(sys.stdin.readline().rstrip(), 16)
     binary = list(f"{hex_in:0>b}")
     binary.reverse()
-    # print(binary)
     n_measurands = int(sys.stdin.readline().rstrip())
     for _ in range(n_measurands):
         datatype, start, size = sys.stdin.readline().rstrip().split(SEPARATOR)
@@ -65,7 +69,6 @@ for caseNum in range(cases):
                     val -= int(arr[i]) * (2**i)
                 else:
                     val += int(arr[i]) * (2**i)
-            # print("".join(binary[-start - size : -start]))
             print(val)
         elif datatype == "uint":
             arr = "".join(reversed(binary[start : start + size]))
@@ -74,13 +77,9 @@ for caseNum in range(cases):
             arr = reversed(binary[start : start + size])
             s = "".join(arr)
             unrounded = bin_to_float(s)
-            # rounded = str(better_round(unrounded, "1.00000"))
-            # first, second = rounded.split("+")
-            print(float_to_scientific(better_round(unrounded, 5)))
+            print(float_to_scientific(unrounded))
         elif datatype == "double":
             arr = reversed(binary[start : start + size])
             s = "".join(arr)
             unrounded = bin_to_double(s)
-            # print(f"{unrounded:f}")
-            # print(f"{unrounded:.5e}")
             print(float_to_scientific(unrounded))
