51fa8e7a7eba74106db8c51f448b46a6b5a5e8cd
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2023-02-25 01:27:36 -0700

    solved morse code

---
+++ morse_code/morse_code.py
@@ -0,0 +1,77 @@
+"""System Module"""
+import sys
+
+
+def words_from_string(line: str):
+    words: list[str] = []
+    start = 0
+    stop = 0
+    for c in line:
+        if c != " ":
+            stop += 1
+        else:
+            words.append(line[start:stop])
+            stop += 1
+            start = stop
+    words.append(line[start:stop])
+    return words
+
+
+def letters_from_morse(line: str):
+    lsep = "   "
+    wsep = "       "
+
+    def find_end(line: str, start: int):
+        for i in range(start, len(line)):
+            if wsep in line[i : i + 7]:
+                return i, 7
+            if lsep in line[i : i + 3]:
+                return i, 3
+        return len(line), len(line)
+
+    morse: list[str] = []
+    start = 0
+    while start < len(line):
+        end, add = find_end(line, start)
+        if line[start:end]:
+            morse.append(line[start:end].lstrip())
+            if add == 7:
+                morse.append(" ")
+        start = end + add
+    return morse
+
+
+def main():
+    letter_to_morse: dict[str, str] = {}
+    morse_to_letter: dict[str, str] = {}
+    cases = int(sys.stdin.readline().rstrip())
+    for _ in range(cases):
+        for _ in range(26):
+            line = sys.stdin.readline().rstrip()
+            letter, morse = line[0], line[2:]
+            letter_to_morse[letter] = morse
+            morse_to_letter[morse] = letter
+        eline = sys.stdin.readline().rstrip()
+        ewords = words_from_string(eline)
+        encoded = ""
+        for word in ewords:
+            for letter in word:
+                encoded += letter_to_morse[letter]
+                encoded += "   "
+            encoded += "    "
+        encoded = encoded.rstrip()
+        print(encoded)
+
+        dline = sys.stdin.readline().rstrip()
+        dwords = letters_from_morse(dline)
+        decoded = ""
+        for word in dwords:
+            if word != " ":
+                decoded += morse_to_letter[word]
+            else:
+                decoded += " "
+        decoded = decoded.rstrip()
+        print(decoded)
+
+
+main()
