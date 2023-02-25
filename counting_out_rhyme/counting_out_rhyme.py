21e24e66e54d318ee935fb34047f9b5aac64c4c3
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2023-02-25 09:22:08 -0700

    solved counting out rhyme

--- counting_out_rhyme/counting_out_rhyme.py
+++ counting_out_rhyme/counting_out_rhyme.py
@@ -9,20 +9,18 @@ def main():
         line = sys.stdin.readline().rstrip()
         people, count = (int(val) for val in line.split(" "))
         peeps = list(range(1, people + 1))
-        for selection in range(people):
+        for start in range(people):  # try each person
             sim = peeps.copy()
-            for round in range(people - 1):
-                # print(sim)
-                # print(f"Value at index {16 % (p + 1)} is", end=" ")
-                # print(sim[16 % (p + 1)])
-                index = (selection + count) % len(sim) - 1
-                # print(selection + 1, index, end=": ")
-                selection = (index + 1) % len(sim)
+            next_person = start
+            for _ in range(people - 1):  # simulate rounds till one person remains
+                # print(f"Starting on person: {sim[next_person]}")
+                index = ((count - 1) + next_person) % len(sim)
+                # print(f"Counting {count} lands on person: {sim[index]}")
                 sim.pop(index)
-                # print(sim.pop(index))
-            print(sim)
-            # if sim[0] == 1:
-            #     print(selection)
+                next_person = (index) % len(sim)
+            # print(f"The remaining person is {sim[0]}")
+            if sim[0] == 1:
+                print(peeps[start])
 
 
 main()
