b4e4f26f627312c30326f5c19f54d5d74132cc8d
Email 6 <loserkid0006@outlook.com>
2023-03-22 14:08:25 -0600

    random cleanup

--- whos_the_valedictorian/whos_the_valedictorian_old.py
+++ whos_the_valedictorian/whos_the_valedictorian_old.py
@@ -3,34 +3,33 @@ import sys
 
 
 class Student:
-    def __init__(self, name: str, gpa: float, credit_hours: float):
+    def __init__(self, name: str, gpa: str, credit_hours: str):
         self.name = name
         self.gpa = gpa
         self.credit_hours = credit_hours
 
 
 def main():
-
     values = {"A": 4, "B": 3, "C": 2, "D": 1}
 
     cases = int(sys.stdin.readline().rstrip())
-    for _ in range(cases):
+    for caseNum in range(cases):
         school = sys.stdin.readline().rstrip()
-        subcases = int(sys.stdin.readline().rstrip())
-        students: list[Student] = []
-        for _ in range(subcases):
+        subCases = int(sys.stdin.readline().rstrip())
+        students = []
+        for _ in range(subCases):
             line = sys.stdin.readline().rstrip()
-            name, classes = (val for val in line.split(":"))
+            name, classes = (val for val in line.split(" "))
             grades = list(val for val in classes.split(","))
             grade_points: int = 0
             credit_hours: int = 0
             for grade in grades:
                 credit_hours += int(grade[1])
-                if x := values.get(grade[0]):
-                    grade_points += x * int(grade[1])
-            gpa: float = grade_points / credit_hours
-            students.append(Student(name, gpa, credit_hours))
-        vale: list[Student] = []
+                temp = values.get(grade[0]) * int(grade[1])
+                grade_points += values.get(grade[0]) * int(grade[1])
+            GPA = grade_points / credit_hours
+            students.append(Student(name, GPA, credit_hours))
+        vale = []
         highest = 0
         for s in students:
             if s.gpa > highest:
@@ -40,7 +39,7 @@ def main():
                 vale.append(s)
         highest = 0
         if len(vale) > 1:
-            for s in vale:
+            for i, s in enumerate(vale):
                 if s.credit_hours > highest:
                     highest = s.credit_hours
             for s in vale:
